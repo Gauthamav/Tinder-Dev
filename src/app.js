477a9defe28ccbe3f888ff0b171b64feec9740a4
Gauthamav <gautham10av@gmail.com>
2025-05-31 22:57:27 +0530

    Set Server for Listening Port 3000

---
+++ src/app.js
@@ -0,0 +1,16 @@
+ 
+
+const express = require('express')
+const app = express()
+
+app.use('/helo',(req,res) => {
+    res.send('hiii')
+})
+app.use('/', (req, res) => {
+  res.send('Hedlddlo ssdld')
+})
+
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000")
+})
