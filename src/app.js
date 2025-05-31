1ee1fdb9c5bf9343ea0964e14e80cc4c35764e55
Gauthamav <gautham10av@gmail.com>
2025-05-31 23:34:37 +0530

    Created Server and Started to Listen to Port 3000

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ const app = express()
 app.use('/helo',(req,res) => {
     res.send('hiii')
 })
+app.use ('/hii',(req,res) => {
+  res.send('hii')
+}) 
 app.use('/', (req, res) => {
   res.send('Hedlddlo ssdld')
 })
+
 
 
 app.listen(3000, () => {
