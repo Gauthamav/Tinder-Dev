273f4bbf5b9f22f2ad5a31311af4b56de67f953d
Gautham <158758292+Gauthamav@users.noreply.github.com>
2025-06-03 00:41:57 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDatabase = async () => {
     try{
         
-        await mongoose.connect("mongodb+srv://gautham10av:hJO7r7uZZAx6P3gw@namastenodejs.htvg49j.mongodb.net/");
+        await mongoose.connect("");
         
     }
     catch(err) {
